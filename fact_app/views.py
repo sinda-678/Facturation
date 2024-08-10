4c8303dbc9cc490e0e94c9f796c7d6a89d24dd37
dora-png <ndoumbedora1094@gmail.com>
2024-08-10 21:54:46 +0100

    creeation de la facture associé au client avec  succes

--- fact_app/views.py
+++ fact_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Customer, Invoice
+from .models import Customer, Invoice,Article
 from django.contrib import messages
+from django.db import transaction
 
 # Vue pour la page d'accueil
 class HomeView(View):
@@ -51,3 +52,64 @@ class AddCustomerView(View):
             messages.error(request, f"Sorry, our system is detecting the following issues: {e}")
 
         return render(request, self.template_name)
+    
+class AddInvoiceView(View):
+    template_name = 'add_invoice.html'
+
+    def get(self, request, *args, **kwargs):
+        customers = Customer.objects.select_related('save_by').all()
+        context = {
+            'customers': customers
+        }
+        return render(request, self.template_name, context)
+    
+    @transaction.atomic()
+    def post(self, request, *args, **kwargs):
+        customers = Customer.objects.select_related('save_by').all()
+        context = {
+            'customers': customers
+        }
+        
+        items = []
+
+        try: 
+            customer = Customer.objects.get(id=request.POST.get('customer'))
+            type = request.POST.get('invoice_type')
+            articles = request.POST.getlist('article')
+            qties = request.POST.getlist('qty')
+            units = request.POST.getlist('unit')
+            total_a = request.POST.getlist('total-a')
+            total = request.POST.get('total')
+            comment = request.POST.get('comment')
+
+            invoice_object = {
+                'customer': customer,
+                'save_by': request.user,
+                'total': total,
+                'invoice_type': type,
+                'comments': comment
+            }
+
+            invoice = Invoice.objects.create(**invoice_object)
+
+            for index, article in enumerate(articles):
+                data = Article(
+                    invoice=invoice,
+                    name=article,
+                    quantity=qties[index],
+                    unit_price=units[index],
+                    total=total_a[index],
+                )
+                items.append(data)
+
+            created = Article.objects.bulk_create(items)   
+
+            if created:
+                messages.success(request, "Data saved successfully.") 
+            else:
+                messages.error(request, "Sorry, please try again, the sent data is corrupt.")    
+
+        except Exception as e:
+            messages.error(request, f"Sorry, the following error has occurred: {e}.")   
+        
+        return render(request, self.template_name, context)
